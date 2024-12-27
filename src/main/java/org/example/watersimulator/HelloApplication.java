b9cff48eaa4fc0d15c06909aeb14d4a1521644c3
Danilo <danimoraga1997@gmail.com>
2024-12-27 15:30:17 +0100

    tile matrix for logic created

--- src/main/java/org/example/watersimulator/HelloApplication.java
+++ src/main/java/org/example/watersimulator/HelloApplication.java
@@ -20,10 +20,16 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    // VARIABLES
+    // static variables
     // Note: Grid will be a square
     static final int GRID_WIDTH = 400; // (in pixels)
     static final int TILES_IN_ROW = 25;
 
+    // Other variables
+    Rectangle tiles[][];
+
+    // METHODS
     public static void main(String[] args) {
         launch();
     }
@@ -31,6 +37,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         createUI(stage);
+
+        tiles[1][2].setFill(Color.BLUE);
     }
 
     public void createUI(Stage stage){
@@ -55,18 +63,27 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    // Creates both UI grid and matrix with references to the tiles (for the logic later on)
     public GridPane initializeGrid(){
         // Calculation for dynamic sizing
         double tileWidth = GRID_WIDTH / (TILES_IN_ROW*1.0);
+
+        // Tiles matrix initialization
+        tiles = new Rectangle[TILES_IN_ROW][TILES_IN_ROW];
 
         // Creation of grid
         GridPane grid = new GridPane();
         for (int i = 0; i < TILES_IN_ROW; i++) {
             for (int j = 0; j < TILES_IN_ROW; j++) {
                 Rectangle tile = new Rectangle(tileWidth, tileWidth, Color.web("#ebebeb"));
+
+                // Construction of the UI
                 tile.setStroke(Color.web("#bfbfbf"));
                 tile.setStrokeWidth(0.5);
                 grid.add(tile, j, i);
+
+                // Construction of matrix that holds the references to the tiles
+                tiles[i][j] = tile;
             }
         }
         return grid;
