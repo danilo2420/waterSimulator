290b731697052d2b6f0e5c9fe2c6b5d83bcff85f
Danilo <danimoraga1997@gmail.com>
2024-12-27 15:14:31 +0100

    basic grid made

--- src/main/java/org/example/watersimulator/HelloApplication.java
+++ src/main/java/org/example/watersimulator/HelloApplication.java
@@ -3,16 +3,24 @@ package org.example.watersimulator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    // Number to decide the number of tiles in the grid
 
     public static void main(String[] args) {
         launch();
@@ -25,20 +33,39 @@ public class HelloApplication extends Application {
 
     public void createUI(Stage stage){
         // Root basic setup
-        VBox root = new VBox(20);
+        VBox root = new VBox(30);
 
         // Creation of elements
         Label label = new Label("Water simulator");
-        // TODO: Gridpane
+        label.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
+
+        // Creation of elements: Gridpane
+        GridPane grid = initializeGrid();
 
         // Add elements to and set up root
-        root.getChildren().setAll(label);
-        root.setPadding(new Insets(20));
+        root.setPadding(new Insets(50));
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().setAll(label, grid);
 
         // Set up UI and show
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public GridPane initializeGrid(){
+        GridPane grid = new GridPane();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                Rectangle tile = new Rectangle(40, 40, Color.web("#ebebeb"));
+                tile.setStroke(Color.web("#bfbfbf"));
+                tile.setStrokeWidth(1);
+                grid.add(tile, j, i);
+            }
+        }
+
+        return grid;
     }
 
 }
