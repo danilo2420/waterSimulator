6617051bc4acbda3837976a9ef6d9bdeb464f837
Danilo <danimoraga1997@gmail.com>
2024-12-27 17:20:59 +0100

    start button added

--- src/main/java/org/example/watersimulator/HelloApplication.java
+++ src/main/java/org/example/watersimulator/HelloApplication.java
@@ -4,9 +4,14 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -25,8 +30,16 @@ public class HelloApplication extends Application {
     static final int TILES_IN_ROW = 25;
     static final String TILE_STARTING_COLOR = "#ebebeb";
 
+    // UI components that have to be global
+    Scene scene;
+    GridPane grid;
+    Button btnStart;
+
     // Other variables
-    Tile tiles[][];
+    Tile[][] tiles;
+    boolean isWPressed = false;
+    boolean isSPressed = false;
+    boolean isSimulationRunning = false;
 
     // METHODS
     public static void main(String[] args) {
@@ -36,28 +49,36 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         createAndShowUI(stage);
-        new Thread(() -> runSimulation() ).start();
+        initializeEvents();
+        // start of logic thread is in the event of btnStart
     }
 
     // UI CONSTRUCTION
     public void createAndShowUI(Stage stage){
         // Root basic setup
-        VBox root = new VBox(30);
+        VBox root = new VBox(15);
 
         // Creation of elements
-        Label label = new Label("Water simulator");
-        label.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
+        Label lblTitle = new Label("Water simulator");
+        lblTitle.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
 
-        // Creation of elements: Gridpane
-        GridPane grid = initializeGrid();
+        VBox VBinstructions = new VBox(5);
+        Label lblInstruction1 = new Label("Hold W and drag to create water cells");
+        Label lblInstruction2 = new Label("Hold S and drag to create solid cells");
+        VBinstructions.setAlignment(Pos.CENTER);
+        VBinstructions.getChildren().setAll(lblInstruction1, lblInstruction2);
+
+        initializeGrid(); // grid is global
+
+        btnStart = new Button("Start simulation");
 
         // Add elements to and set up root
         root.setPadding(new Insets(50));
         root.setAlignment(Pos.CENTER);
-        root.getChildren().setAll(label, grid);
+        root.getChildren().setAll(lblTitle, VBinstructions, grid, btnStart);
 
         // Set up UI and show
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> {
             Platform.exit();
@@ -67,7 +88,7 @@ public class HelloApplication extends Application {
     }
 
     // Creates both UI grid and matrix with references to the tiles (for the logic later on)
-    public GridPane initializeGrid(){
+    public void initializeGrid(){
         // Calculation for dynamic sizing
         double tileWidth = GRID_WIDTH / (TILES_IN_ROW*1.0);
 
@@ -75,7 +96,7 @@ public class HelloApplication extends Application {
         tiles = new Tile[TILES_IN_ROW][TILES_IN_ROW];
 
         // Creation of grid
-        GridPane grid = new GridPane();
+        grid = new GridPane();
         for (int i = 0; i < TILES_IN_ROW; i++) {
             for (int j = 0; j < TILES_IN_ROW; j++) {
                 Tile tile = new Tile(tileWidth);
@@ -86,13 +107,80 @@ public class HelloApplication extends Application {
                 tiles[i][j] = tile;
             }
         }
-        return grid;
+    }
+
+    // EVENTS
+    public void initializeEvents(){
+        // Events to check whether a key is pressed (necessary later)
+        checkKeysPressed();
+
+        // Events to change the state of the cells
+        for(Node node : grid.getChildren()){
+            int i = GridPane.getRowIndex(node);
+            int j = GridPane.getColumnIndex(node);
+
+            Tile tile = tiles[i][j];
+            tile.getRectangle().setOnMouseEntered(event -> {
+                if(isWPressed)
+                    tile.setState(TileState.WATER);
+                if(isSPressed)
+                    tile.setState(TileState.SOLID);
+            });
+        }
+
+        // Event for the start button
+        btnStart.setOnAction(event -> {
+            new Thread(() -> runSimulation() ).start();
+        });
+    }
+
+    // this method allows us to know if a key is pressed (the left click button doesnt work well for some reason)
+    public void checkKeysPressed(){
+        scene.setOnKeyPressed(event -> {
+            switch(event.getCode()){
+                case KeyCode.W:
+                    isWPressed = true;
+                    System.out.println("W is pressed");
+                    break;
+                case KeyCode S:
+                    isSPressed = true;
+                    System.out.println("S is pressed");
+                    break;
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            switch(event.getCode()){
+                case KeyCode.W:
+                    isWPressed = false;
+                    System.out.println("W is released");
+                    break;
+                case KeyCode S:
+                    isSPressed = false;
+                    System.out.println("S is released");
+                    break;
+            }
+        });
     }
 
     // LOGIC
     public void runSimulation(){
-        for (int i = 0; i < TILES_IN_ROW; i++) {
-            for (int j = 0; j < TILES_IN_ROW; j++) {
+        while(true) {
+            for (int i = 0; i < TILES_IN_ROW; i++) {
+                for (int j = 0; j < TILES_IN_ROW; j++) {
+                    Tile tile = tiles[i][j];
+                    TileState originalState = tile.getState();
+                    switch (originalState) {
+                        case EMPTY:
+                            tile.setState(TileState.WATER);
+                            break;
+                        case SOLID:
+                            tile.setState(TileState.EMPTY);
+                            break;
+                        case WATER:
+                            tile.setState(TileState.SOLID);
+                    }
+
+                /*
                 washGrid();
                 tiles[i][j].setState(TileState.WATER);
                 try {
@@ -100,7 +188,11 @@ public class HelloApplication extends Application {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+
+                 */
+                }
             }
+            pause(1000);
         }
     }
 
@@ -108,5 +200,13 @@ public class HelloApplication extends Application {
         for(Tile[] row : tiles)
             for(Tile tile : row)
                 tile.setState(TileState.EMPTY);
+    }
+
+    public void pause(int milliseconds){
+        try {
+            Thread.sleep(milliseconds);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
